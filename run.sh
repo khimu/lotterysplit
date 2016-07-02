e6a998ccea0b941fe617a40783c3dacca72e7f16
khimung <kim2kim@gmail.com>
2016-07-02 12:31:27 -0700

    WORKING - Remove old jetty-pkg from root pom.xml;

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 mvn clean install -DskipTests=true -Pprod
 #scp lotterysplit/target/lotterysplit.war root@107.170.234.144:/var/lib/tomcat7/webapps/lotto.war
-scp jetty-pkg/target/lottery.jar root@107.170.234.144:/opt/lottery/
+
+# this wont work on digitalocean 
+#scp jetty-pkg/target/lottery.jar root@107.170.234.144:/opt/lottery/
+
+# switching to this and hopefully will work on digitalocean
+scp lotterysplit/target/lottery.war root@107.170.234.144:/opt/lottery/lottery.war
+scp lotterysplit/target/dependency/jetty-runner.jar root@107.170.234.144:/opt/lottery/jetty-runner.jar
